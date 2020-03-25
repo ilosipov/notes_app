a5eba26d2a632e1c253ecd2946d1bd509ffcf4bb
Ilya Osipov <il.osipov.ya@yandex.ru>
2020-03-25 12:01:51 +0300

    added bottom bar in MainFragment. notes app

--- app/src/main/java/com/job4j/notesapp/fargment/MainFragment.kt
+++ app/src/main/java/com/job4j/notesapp/fargment/MainFragment.kt
@@ -2,10 +2,11 @@ package com.job4j.notesapp.fargment
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.job4j.notesapp.R
 
 /**
@@ -18,11 +19,42 @@ import com.job4j.notesapp.R
 class MainFragment : Fragment() {
     private val log = "MainFragment"
 
+    private lateinit var bottomBar : BottomAppBar
+    private lateinit var addBtn : FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         Log.d(log, "onCreateView: initialization MainFragment.")
 
+        bottomBar = view.findViewById(R.id.bottom_bar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(bottomBar)
+        addBtn = view.findViewById(R.id.btn_add)
+        addBtn.setOnClickListener(this::onClickAdd)
+
         return view
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun onClickAdd(v: View) {
+        Log.d(log, "onClickAdd: click button.")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btn_left -> { Log.d(log, "onClick: click btn left.") }
+            R.id.btn_right -> { Log.d(log, "onClick: click btn right.") }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
